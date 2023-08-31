740f17ef990d0aa0c9f8ffc4fbeaf9ba0367fb1f
Eddwiin <maguran.mahendrarajan@gmail.com>
2023-08-31 23:31:05 +0200

    add dockerfile

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import http from 'http';
+
+const hostname = process.env.HOSTNAME ||'127.0.0.1';
+const port = Number(process.env.PORT) || 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log("Server running")
+})
